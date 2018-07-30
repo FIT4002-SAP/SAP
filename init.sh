34cb84f3da6889e1b64a8f834bf95124ede194fc
Benjamin Tanone <benjamin.tanone@gmail.com>
2018-07-30 16:59:26 +1000

    update readme on pull requests and dependencies & add init.sh (#2)
    added init.sh onto readme.md, along with s3 link, and updated readme

---
+++ init.sh
@@ -0,0 +1,14 @@
+set -e
+
+ZIP_URL=https://s3-ap-southeast-2.amazonaws.com/fit4002-intelligence-content-2/frameworks.zip;
+
+echo 'Downloading dependencies...';
+curl -o frameworks.zip $ZIP_URL;
+echo 'Extracting dependencies...';
+unzip -n frameworks.zip -d frameworks.tmp;
+echo 'Moving dependencies...';
+mv frameworks.tmp/*.framework FIT4002-v1;
+echo 'Cleaning up...';
+rm -R frameworks.tmp;
+rm frameworks.zip;
+echo 'Finished!';
