ccbbad94d10cead1cbfae967dd20a87b3b469ad0
Ben <benjamin.tanone@gmail.com>
2018-07-30 12:29:41 +1000

    changed gdrive link to s3

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 set -e
 
-ZIP_URL=https://doc-0g-3k-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/5ltknhkjtoppq42p2la17aub7ak0u9l3/1532865600000/10713371595697615877/*/1eoNza-pl9REl77vCTxnxqFMZIxTVc9-F?e=download;
+ZIP_URL=https://s3-ap-southeast-2.amazonaws.com/fit4002-intelligence-content-2/frameworks.zip;
 
 echo 'Downloading dependencies...';
 curl -o frameworks.zip $ZIP_URL;
